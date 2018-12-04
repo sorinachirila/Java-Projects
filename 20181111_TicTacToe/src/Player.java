48d9c6e6038c541f28e7f36a39107926044e1410
Sorina <chirila_sorina@yahoo.com>
2018-12-04 15:51:27 +0200

    adaugare comentariu in cod pentru clasa Player

--- 20181111_TicTacToe/src/Player.java
+++ 20181111_TicTacToe/src/Player.java
@@ -1,11 +1,11 @@
 public class Player {
-
+    //definire proprietati pentru un jucator/player
     String name;
-
     char symbol;
 
-    //constructor
+    //constructor cu doi parametri
     public Player(String name, char symbol) {
+        //atribuire/initializare cu valori pentru proprietatile jucatorului
         this.name = name;
         this.symbol = symbol;
     }
