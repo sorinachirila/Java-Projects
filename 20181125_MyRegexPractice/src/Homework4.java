339eadd5ed3243b65e8f3b715b8f726bb74aab12
Sorina <chirila_sorina@yahoo.com>
2018-12-07 21:34:24 +0200

    Homework 4 simple regex exercises

--- 20181125_MyRegexPractice/src/Homework4.java
+++ 20181125_MyRegexPractice/src/Homework4.java
@@ -1,7 +1,7 @@
 public class Homework4 {
     public static void main(String[] args) {
         String [] myStringsArray = new String[]{"Hello World", "Hey", "How are you?"};
-
+        //testing
         System.out.println(myStringsArray[0].matches("Hello World")); //true
         System.out.println(myStringsArray[1].matches("[^a-zA-Z]*")); //false
         System.out.println(myStringsArray[2].matches("[a-zA-Z]+")); //false
